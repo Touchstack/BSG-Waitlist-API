92829d7621bd4900ec796a903cb880accd8f9ae0
Eugene Dumoga <eugenedumoga@gmail.com>
2023-12-12 08:08:34 +0000

    Update user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,21 +1,33 @@
-const status = require('http-status');
-const has = require('has-keys');
-const db = require('../models/database.js');
-const schema = require('../models/users.js');
-  
-
-
+const status = require("http-status");
+const has = require("has-keys");
+const db = require("../models/database.js");
+const schema = require("../models/users.js");
 
 module.exports = {
-    async newUser(req, res){
-        if(!has(req.body, ['email']))
-            throw {code: status.BAD_REQUEST, message: 'You must specify the email'};
+  async newUser(req, res) {
+    if (!has(req.body, ["email"]))
+      throw { code: status.BAD_REQUEST, message: "You must specify the email" };
 
-        let { email } = req.body;
-        await schema.validateAsync({email});
-        await db.getDbo().then((result)=>{
-            result.collection('join').insertOne({email})
-           });
-            res.status(200).json({status: true, message: 'joined'});
-    }
-}
+    let { email } = req.body;
+    await schema.validateAsync({ email });
+    await db
+      .getDbo()
+      .then((result) => {
+        result.collection("join").createIndex({ email: 1 }, { unique: true });
+        result.collection("join").insertOne({ email });
+      })
+      .catch((err) => {
+        if (err) {
+          if (err.code === 11000) {
+            // Duplicate key error email is not unique
+            return res.status(400).json({
+              status: status.BAD_REQUEST,
+              error: "Your email has already been added to the WaitList",
+            });
+          }
+          return res.status(500).json({ error: "Internal server error" });
+        }
+      });
+    res.status(200).json({ status: true, message: "joined" });
+  },
+};
