94d2976d0772081fbd0c597581d6c39bdf91e36d
Eugene Dumoga <eugenedumoga@gmail.com>
2023-12-14 03:13:42 +0000

    Removed email validation

--- src/models/users.js
+++ src/models/users.js
@@ -1,10 +1,7 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const schema = Joi.object({
-   email: Joi.string()
-     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-     .required()
- });
-
+  email: Joi.string().email().required(),
+});
 
 module.exports = schema;
